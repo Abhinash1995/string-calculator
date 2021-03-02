ea3d2558b9b70fb57b8daf6856fddfe6264268b1
Abhinash <Atanas.atansov@sunrise.net>
2021-03-02 22:48:51 +0530

    test case for empty string should return zero

---
+++ src/test/StringCalculatorTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import main.StringCalculator;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class StringCalculatorTest {
+
+    private StringCalculator calculator;
+
+    @Before
+    public void initialize() {
+        calculator = new StringCalculator();
+    }
+
+    @Test
+    public void emptyStringShouldReturn0() {
+        assertEquals(calculator.add(""), 0);
+    }
+}
